fd6c24f69563158b02ba423c267660337a9c9c60
Thorsten Kurth <tkurth@nvidia.com>
2024-04-17 04:55:46 -0700

    enhancing ignre_tkr

--- src/fsrc/torchfort_m.F90
+++ src/fsrc/torchfort_m.F90
@@ -1957,6 +1957,40 @@ contains
   end function torchfort_rl_off_policy_evaluate_float_4d_dev
 #endif
 
+  function torchfort_rl_off_policy_evaluate_double_4d(mname, state, act, reward, stream) result(res)
+    character(len=*) :: mname
+    real(real64) :: state(:, :, :, :), act(:, :, :, :), reward(:, :)
+    integer(int64), optional :: stream
+    integer(c_int) :: res
+
+    integer(int64) :: stream_
+
+    integer(c_size_t) :: state_dim, act_dim, reward_dim
+
+    state_dim = size(shape(state))
+    act_dim = size(shape(act))
+    reward_dim = size(shape(reward))
+
+    stream_ = 0
+    if (present(stream)) stream_ = stream
+
+    block
+      integer(c_int64_t) :: state_shape(state_dim)
+      integer(c_int64_t) :: act_shape(act_dim)
+      integer(c_int64_t) :: reward_shape(reward_dim)
+
+      state_shape(:) = shape(state)
+      act_shape(:) = shape(act)
+      reward_shape(:) = shape(reward)
+
+      res = torchfort_rl_off_policy_evaluate_c([trim(mname), C_NULL_CHAR], &
+                                               state, state_dim, state_shape, &
+                                               act, act_dim, act_shape, &
+                                               reward, reward_dim, reward_shape, &
+                                               TORCHFORT_DOUBLE, stream_)
+    end block
+  end function torchfort_rl_off_policy_evaluate_double_4d
+
   ! RL on-policy related routines
   ! logging
   function torchfort_rl_on_policy_wandb_log_int(mname, metric_name, step, val) result(res)

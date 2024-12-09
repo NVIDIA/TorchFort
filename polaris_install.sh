8e0f926f1ef5e84dc26dedff7b34a481082cc3f5
Riccardo Strina <riccardo.strina@icloud.com>
2024-12-09 23:41:03 +0100

    Add MPICH env

--- polaris_install.sh
+++ polaris_install.sh
@@ -3,6 +3,7 @@
 # Since CUDA Aware-MPI is being used the following env must be set and module loaded
 export MPICH_GPU_SUPPORT_ENABLED=1 && \
 export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 && \
+export MPICH_RDMA_ENABLED_CUDA=1 && \
 module load craype-accel-nvidia80 && \
 cmake -DCMAKE_INSTALL_PREFIX=/home/tartarughina/TorchFort-def \
 -DTORCHFORT_BUILD_EXAMPLES=1 \
@@ -17,4 +18,5 @@ cmake -DCMAKE_INSTALL_PREFIX=/home/tartarughina/TorchFort-def \
 export LD_LIBRARY_PATH=/soft/libraries/libtorch/libtorch-2.4.0+cu124/lib:$HOME/hdf5/build/HDF5-1.17.0-Linux/HDF_Group/HDF5/1.17.0/lib:$HOME/TorchFort-def/lib:$LD_LIBRARY_PATH && \
 export MPICH_GPU_SUPPORT_ENABLED=1 && \
 export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 && \
+export MPICH_RDMA_ENABLED_CUDA=1 && \
 module load craype-accel-nvidia80

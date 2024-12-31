56491bc800cbb35fc9e32663bbf6a32255f949c9
Riccardo Strina <riccardo.strina@icloud.com>
2024-12-31 22:05:54 +0100

    Remove yaml cpp link to manually installed version

--- polaris_install.sh
+++ polaris_install.sh
@@ -4,6 +4,7 @@
 export MPICH_GPU_SUPPORT_ENABLED=1 && \
 export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 && \
 export MPICH_RDMA_ENABLED_CUDA=1 && \
+export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH && \
 module load craype-accel-nvidia80 && \
 module load cray-hdf5/1.12.2.9 && \
 cmake -DCMAKE_INSTALL_PREFIX=/home/tartarughina/TorchFort-def \
@@ -11,13 +12,11 @@ cmake -DCMAKE_INSTALL_PREFIX=/home/tartarughina/TorchFort-def \
 -DCMAKE_PREFIX_PATH=/soft/libraries/libtorch/libtorch-2.4.0+cu124/share/cmake \
 ..
 
-# Unused options
-# -DTORCHFORT_YAML_CPP_ROOT=/home/tartarughina/yaml-cpp-def \
-# -DCMAKE_PREFIX_PATH="`python3.11 -c 'import torch;print(torch.utils.cmake_prefix_path)'`" \
 
 # In order to run the program the following commands need to be executed
-export LD_LIBRARY_PATH=/soft/libraries/libtorch/libtorch-2.4.0+cu124/lib:$HOME/hdf5/build/HDF5-1.17.0-Linux/HDF_Group/HDF5/1.17.0/lib:$HOME/TorchFort-def/lib:$LD_LIBRARY_PATH && \
+export LD_LIBRARY_PATH=/soft/libraries/libtorch/libtorch-2.4.0+cu124/lib:$HOME/TorchFort-def/lib:$LD_LIBRARY_PATH && \
 export MPICH_GPU_SUPPORT_ENABLED=1 && \
 export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 && \
 export MPICH_RDMA_ENABLED_CUDA=1 && \
-module load craype-accel-nvidia80
+module load craype-accel-nvidia80 && \
+module load cray-hdf5/1.12.2.9

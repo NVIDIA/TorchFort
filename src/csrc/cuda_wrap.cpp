ff96d77eb4de132c3d414e905319a9d963feda6d
Thorsten Kurth <tkurth@nvidia.com>
2026-07-08 15:39:02 +0200

    CUDA 13.2 and torch 2.12.1 compatible Dockerfile. Volta (SM70) support dropped (#110)
    * CUDA 13.2 and torch 2.12.1 compatible Dockerfiles. Volta (SM70) support in container dropped, but code can still be compiled against older CUDA and with 7.0 support.
    
    Signed-off-by: Thorsten Kurth <tkurth@nvidia.com>

--- src/csrc/cuda_wrap.cpp
+++ src/csrc/cuda_wrap.cpp
@@ -25,8 +25,8 @@
 #define LOAD_SYM(symbol, version, optional)                                                                            \
   do {                                                                                                                 \
     cudaDriverEntryPointQueryResult driverStatus = cudaDriverEntryPointSymbolNotFound;                                 \
-    cudaError_t err = cudaGetDriverEntryPointByVersion(#symbol, (void**)(&cuFnTable.pfn_##symbol), version,   \
-                                                       cudaEnableDefault, &driverStatus));                             \
+    cudaError_t err = cudaGetDriverEntryPointByVersion(#symbol, (void**)(&cuFnTable.pfn_##symbol), version,            \
+                                                       cudaEnableDefault, &driverStatus);                              \
     if ((driverStatus != cudaDriverEntryPointSuccess || err != cudaSuccess) && !optional) {                            \
       THROW_CUDA_ERROR("cudaGetDriverEntryPointByVersion failed.");                                                    \
     }                                                                                                                  \
